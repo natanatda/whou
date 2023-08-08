47bcfe28e919a048b26c18b3b331969169633179
psh <>
2023-08-08 15:01:34 +0900

    관리자페이지 검색어 차트&리스트

--- whou/src/main/java/whou/secproject/component/AssistantDTO.java
+++ whou/src/main/java/whou/secproject/component/AssistantDTO.java
@@ -1,9 +1,12 @@
 package whou.secproject.component;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
+@SuppressWarnings("serial")
 @Data
-public class AssistantDTO{	
+public class AssistantDTO implements Serializable{	
 	private String qes; // 질문
 	private String con; // 답변
 	private int num; // 번호
